fa3b0fb517b6f546335a9fc7eac3be6375ccbc38
Umarfarooq J Ron <umarron.10@gmail.com>
2026-01-19 12:08:42 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,24 +1,20 @@
-def check_number(num):
-    # Check Even or Odd
+def n(num):
     if num % 2 == 0:
         print("Even")
     else:
         print("Odd")
 
-    # Check Prime
+
+if __name__ == "__main__":
+    num = 7
     if num <= 1:
         print("Not Prime")
-        return
-
     for i in range(2, num):
         if num % i == 0:
             print("Not Prime")
             break
     else:
         print("Prime")
+    n(num)
 
-
-if __name__ == "__main__":
-    num = 7
-    check_number(num)
 
