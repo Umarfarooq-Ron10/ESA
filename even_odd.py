68f20aea7281ae5f97cb6de9fe5b996f188f5306
Umarfarooq J Ron <umarron.10@gmail.com>
2026-01-19 11:48:10 +0530

    Update even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,12 @@
+def n(num):
+    odd=(num %2==0)
+    prime=(num % i!=0)
+    
+if __name__=="__main__":
+   num=7
+   eo=(num %2 ==0)
+   print("Even_odd:",eo)
+   prime=(num % i!=0)
+   print("prime")
+else:
+   print("Not prime")
